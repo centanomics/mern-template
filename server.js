2e503646258e10cdb695b090002a0d3f2dea835b
semyers189 <semyers@fullsail.edu>
2021-02-12 21:10:37 -0500

    made template that should take care of everything

---
+++ server.js
@@ -0,0 +1,34 @@
+// dont forget to create an .env file
+const cors = require('cors');
+const express = require('express');
+
+//define needed files here
+
+const app = express();
+
+app.use(cors());
+
+//connect database
+
+//init middleware
+app.use(express.json({ extended: false }));
+
+//define routes
+
+
+//Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set Static Folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  );
+}
+
+//Define Ports
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server started on ${PORT}`);
+});
